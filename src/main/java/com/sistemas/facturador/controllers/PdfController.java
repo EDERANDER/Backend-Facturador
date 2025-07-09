4769d6315e7c684ad79c62cd6b61e013b854675e
EDERANDER <ea.quispe@est.unap.edu.pe>
2025-07-08 21:37:02 -0500

    agregando funcion de traer un documento electronico atravez de su correlativo y serie

--- src/main/java/com/sistemas/facturador/controllers/PdfController.java
+++ src/main/java/com/sistemas/facturador/controllers/PdfController.java
@@ -1,5 +1,6 @@
 package com.sistemas.facturador.controllers;
 
+import com.sistemas.facturador.models.InvoiceRequest;
 import com.sistemas.facturador.services.PdfService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,19 @@ public class PdfController {
     public ResponseEntity<byte[]> descargarUltimoPdf() {
         try {
             byte[] pdf = pdfService.obtenerUltimoPdf();
+            return ResponseEntity.ok()
+                    .header("Content-Type", "application/pdf")
+                    .header("Content-Disposition", "inline; filename=\"comprobante.pdf\"")
+                    .body(pdf);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @PostMapping("/devolverEspecifico")
+    public ResponseEntity<byte[]> descargarEspecifico(@RequestBody InvoiceRequest request) {
+        try {
+            byte[] pdf = pdfService.obtenerPdfEspecifico(request);
             return ResponseEntity.ok()
                     .header("Content-Type", "application/pdf")
                     .header("Content-Disposition", "inline; filename=\"comprobante.pdf\"")
--- src/main/java/com/sistemas/facturador/services/PdfService.java
+++ src/main/java/com/sistemas/facturador/services/PdfService.java
@@ -1,6 +1,7 @@
 package com.sistemas.facturador.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sistemas.facturador.models.InvoiceRequest;
 import com.sistemas.facturador.models.NumeracionHistorial;
 import com.sistemas.facturador.repositories.NumeracionHistorialRepository;
 import kong.unirest.HttpResponse;
@@ -35,6 +36,26 @@ public class PdfService {
         body.put("tipo_Doc", ultimo.getTipoDocumento());
         body.put("serie", ultimo.getSerie());
         body.put("correlativo", String.valueOf(Integer.parseInt(ultimo.getCorrelativo())));
+
+        HttpResponse<byte[]> response = Unirest.post("https://apife-qa.factiliza.com/api/v1/invoice/pdf")
+                .header("Authorization", "Bearer " + factilizaToken)
+                .header("Content-Type", "application/json")
+                .body(new ObjectMapper().writeValueAsString(body))
+                .asBytes();
+
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("Error al obtener PDF: " + response.getStatusText());
+        }
+
+        return response.getBody();
+    }
+
+    public byte[] obtenerPdfEspecifico(InvoiceRequest request) throws Exception {
+        Map<String, Object> body = new HashMap<>();
+        body.put("empresa_Ruc", "20607086215");
+        body.put("tipo_Doc", request.getTipo_Doc());
+        body.put("serie", request.getSerie());
+        body.put("correlativo", request.getCorrelativo());
 
         HttpResponse<byte[]> response = Unirest.post("https://apife-qa.factiliza.com/api/v1/invoice/pdf")
                 .header("Authorization", "Bearer " + factilizaToken)
